4b17ccf86cbd5b28df05ce8e05f80e5e593412df
anusuyadevi-sb <anusuyadevi.s.b@ibm.com>
2022-01-18 20:36:04 +0530

    code

---
+++ src/main/java/com/apmm/myFirstProject/MyFirstProjectApplication.java
@@ -0,0 +1,25 @@
+package com.apmm.myFirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyFirstProjectApplication {
+	@GetMapping("/Welcome")
+	public String welcome(){
+		return "Welcome to my first project";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectApplication.class, args);
+	}
+	/*echo "# myFirstProject" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/anusuyadevi-sb/myFirstProject.git
+	git push -u origin main*/
+}
