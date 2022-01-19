305ffe9a8bbfefa357b3a9817ce8afa5f0510c61
anusuyadevi-sb <97877559+anusuyadevi-sb@users.noreply.github.com>
2022-01-20 02:09:13 +0530

    Update MyFirstProjectApplication.java

--- src/main/java/com/apmm/myFirstProject/MyFirstProjectApplication.java
+++ src/main/java/com/apmm/myFirstProject/MyFirstProjectApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyFirstProjectApplication {
 	@GetMapping("/Welcome")
 	public String welcome(){
-		return "Welcome to my first project";
+		return "Great Anu!!you rockz...";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectApplication.class, args);
